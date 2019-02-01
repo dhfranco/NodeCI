e1c5cf0a9375196de54f700e3c526c626bfe2263
dhfranco <37462717+dhfranco@users.noreply.github.com>
2019-02-01 08:14:39 -0800

    stop tracking config files

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://devtest:devtest1@ds035633.mlab.com:35633/blog_dev',
-  cookieKey: '123123123',
-  redisUrl: 'localhost://127.0.0.1:6379'
-};
